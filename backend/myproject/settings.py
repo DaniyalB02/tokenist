c6144a942babc1157e4c2cc420e6ce888eec7892
Shoaib Anwar <shoaib.anwar@hotmail.com>
2023-12-07 00:25:39 +0100

    Update django-heroku with new library

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import django_heroku 
+import django_on_heroku 
 import dj_database_url
 from pathlib import Path
 
@@ -161,4 +161,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
